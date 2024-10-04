be75bb2a53e366dd3408c495fc82748ac7eba13f
luchop27 <xkrules.2005@gmail.com>
2024-10-04 00:58:05 -0500

    cambio4

--- zatuarApp/settings.py
+++ zatuarApp/settings.py
@@ -131,10 +131,10 @@ EMAIL_PORT = 587
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT='/var/www/zatuarApp/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 #STATICFILES_DIRS=[
 #    'static',
 #]
 
 MEDIA_URL="/media/"
-MEDIA_ROOT='/var/www/zatuarApp/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
